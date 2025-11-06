8adcda175229d316e228310d265d631f31ba2d1c
Laísa Dummer <116893465+lapdummer@users.noreply.github.com>
2025-11-05 21:06:51 -0300

    Informações !!!

---
+++ src/main/java/humanize/service/controllers/InformationController.java
@@ -0,0 +1,12 @@
+package humanize.service.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/information")
+@AllArgsConstructor
+
+public class InformationController {
+}
